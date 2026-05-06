2ef33a6fd874c3267b8c1b8e64c2bcb19392d53b
saketh-nandu <mantolsaketh@gmail.com>
2026-05-06 17:31:22 +0530

    Fix Android build: Update compileSdk to 36, ndkVersion to 27.0.12077973, and minSdk to 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 
 android {
     namespace = "com.company.compass"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -28,8 +28,8 @@ android {
         applicationId = "com.company.compass"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
